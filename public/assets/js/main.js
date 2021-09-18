e8afa0071a85cce95e2b171844fa2ebd0be5829a
Sanjeevlu Buggargani <35345738+nsanju0413@users.noreply.github.com>
2021-09-19 00:40:02 +0530

    Contact Form Added

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: eStartup - v4.4.0
-* Template URL: https://bootstrapmade.com/estartup-bootstrap-landing-page-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
@@ -214,3 +208,64 @@
   });
 
 })()
+
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+  apiKey: "AIzaSyBeAA0M1l570a4lk2He-2EyvBctwWQ7VmI",
+  authDomain: "ksmdrivings.firebaseapp.com",
+  databaseURL: "https://ksmdrivings-default-rtdb.firebaseio.com",
+  projectId: "ksmdrivings",
+  storageBucket: "ksmdrivings.appspot.com",
+  messagingSenderId: "315460566066",
+  appId: "1:315460566066:web:9727b58d9aa05a0914502b",
+  measurementId: "G-3406VW7K4N"  
+ 
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
